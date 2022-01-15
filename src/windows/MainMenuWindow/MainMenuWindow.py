2a9a3d077362624fc749fee090288d92b7da79c7
khanbin <totoshka1588@gmail.com>
2022-01-15 20:43:14 +0600

    Обновил Button.py

--- src/windows/MainMenuWindow/MainMenuWindow.py
+++ src/windows/MainMenuWindow/MainMenuWindow.py
@@ -4,10 +4,11 @@ from windows.Window import Window
 
 from handlers.Button import Button
 from handlers.TextHandler import TextHandler
+from handlers.ImageHandler import ImageHandler
 
 from constants.windows.windows_names import START_WINDOW_NAME
 from constants.windows.main_menu_window_settings.main_menu_window_settings \
-    import BUTTON_SIZE
+    import BUTTON_SIZE, OPEN_LEVELS_LIST_WINDOW_BUTTON_PATH
 
 
 class MainMenuWindow(Window):
@@ -24,8 +25,11 @@ class MainMenuWindow(Window):
         button_size = self.get_screen().get_width() * self.get_button_width(),\
                       self.get_screen().get_height() * self.get_button_height()
 
-        Button((100, 200), button_size,
-               (self.flip_window, (self.get_parent().get_windows()[START_WINDOW_NAME],), {}), buttons)
+        Button((100, 200), (self.flip_window, (
+            self.get_parent().get_windows()[START_WINDOW_NAME],), {}),
+               pygame.transform.scale(ImageHandler.load_image(
+                   OPEN_LEVELS_LIST_WINDOW_BUTTON_PATH, -1), button_size),
+               buttons)
 
     def render(self, screen: pygame.Surface):
         super().render(screen)
