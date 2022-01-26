54233b974056a6ba83d610da52b8792f1605375f
ScareTrow <75736890+ScareTrow@users.noreply.github.com>
2022-01-27 05:54:54 +0600

    pep-8 formatting

--- src/Animations/hero_animation.py
+++ src/Animations/hero_animation.py
@@ -13,6 +13,7 @@ running_left = 20
 idle_right = 29
 idle_left = 11
 
+
 class AnimatedSprite:
     def __init__(self, screen, x, y, frames):
         self.screen = screen
@@ -27,15 +28,14 @@ class AnimatedSprite:
         self.dwell_countdown -= 1
         if self.dwell_countdown < 0:
             self.dwell_countdown = dwell
-            self.index = (self.index+1)%(frame_set_end+1)
-            if self.index<frame_set_start:
+            self.index = (self.index + 1) % (frame_set_end + 1)
+            if self.index < frame_set_start:
                 self.index = frame_set_start
 
     def draw(self):
-        self.screen.blit(self.frames[self.index],
-                     ( int(self.x-self.rect.width/2),
-                       int(self.y-self.rect.height/2) ))
-
+        self.screen.blit(self.frames[self.index], (
+                         int(self.x - self.rect.width / 2),
+                         int(self.y - self.rect.height / 2)))
 
 
 def strip_from_sheet():
@@ -50,25 +50,27 @@ def strip_from_sheet():
     frames = []
     for row in range(rows):
         for col in range(columns):
-            rect = pygame.Rect(col*img_width, row*img_height, img_width, img_height)
+            rect = pygame.Rect(
+                col * img_width, row * img_height, img_width, img_height)
             frames.append(sheet.subsurface(rect))
 
     for row in range(rows):
         for col in range(columns):
-            rect = pygame.Rect(col*img_width, row*img_height, img_width, img_height)
-            frames.append(pygame.transform.flip(sheet.subsurface(rect), True, False))
+            rect = pygame.Rect(
+                col * img_width, row * img_height, img_width, img_height)
+            frames.append(
+                pygame.transform.flip(sheet.subsurface(rect), True, False))
 
     return frames
 
 
-
 clock = pygame.time.Clock()
-screen = pygame.display.set_mode((800,600))
+screen = pygame.display.set_mode((800, 600))
 
 frames = strip_from_sheet()
 dimensions = frames[0].get_rect()
 scaling = 1
-dimensions = (int(dimensions.w*scaling),int(dimensions.h*scaling))
+dimensions = (int(dimensions.w * scaling), int(dimensions.h * scaling))
 
 for i in range(len(frames)):
     frames[i] = pygame.transform.scale(
@@ -78,19 +80,20 @@ for i in range(len(frames)):
 sprite = AnimatedSprite(screen, 200, 300, frames)
 
 
-image = pygame.image.load(r"Yandex-project-develop\src\data\images\animations\Hero_Move.png").convert()
+image = pygame.image.load(
+    r"Yandex-project-develop\src\data\images\animations\Hero_Move.png").convert(
+        )
 rect = pygame.Rect(100, 100, 100, 100)
 random_image = image.subsurface(rect)
-
 
 
 done = False
 while not done:
     pressed = pygame.key.get_pressed()
     if pressed[pygame.K_LEFT]:
-        sprite.x-=speed
+        sprite.x -= speed
     if pressed[pygame.K_RIGHT]:
-        sprite.x+=speed
+        sprite.x += speed
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
@@ -111,10 +114,10 @@ while not done:
                 set_index = idle_right
             elif event.key == pygame.K_LEFT:
                 set_index = idle_left
-        frame_set_start = set_index*3
-        frame_set_end = frame_set_start+2
+        frame_set_start = set_index * 3
+        frame_set_end = frame_set_start + 2
 
-    screen.fill((0,0,0))
+    screen.fill((0, 0, 0))
     sprite.draw()
     sprite.advanceImage()
 
