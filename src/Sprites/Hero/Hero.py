4d95582bc8aeea35de5dc3f90c667c55e21b0d08
khanbin <totoshka1588@gmail.com>
2022-01-27 13:31:35 +0600

    Добавил нижний хитбокс к Hero.py, скольжение по вертикальным поверхностям и свойство hook нужное для прыжков от стен

--- src/Sprites/Hero/Hero.py
+++ src/Sprites/Hero/Hero.py
@@ -6,6 +6,7 @@ from handlers.ConvertHandler.ConvertHandler import ConvertHandler
 from handlers.ImageHandler.ImageHandler import ImageHandler
 
 from Sprites.Sprite import Sprite
+from Sprites.Wall.Wall import Wall
 
 
 class Hero(Sprite):
@@ -19,10 +20,13 @@ class Hero(Sprite):
 
         self.image = pygame.transform.scale(
             ImageHandler.load_image(IMAGE_PATH, -1), self.get_rect().size)
+        self.mask = pygame.mask.from_surface(self.get_image())
 
         self.setup_step()
         self.vector = 0, 0
         self.inertia = 0, 0
+
+        self.hook = False
 
     def setup_step(self):
         self.step = \
@@ -34,21 +38,122 @@ class Hero(Sprite):
                 self.jump()
 
     def tick(self):
+        """Вызывается при каждом тике игрового цикла"""
         pressed_keys = pygame.key.get_pressed()
 
         if pressed_keys[pygame.K_a]:
-            self.set_rect(self.get_rect().move(-1 * self.get_step(), 0))
+            self.move((-1 * self.get_step(), 0))
         elif pressed_keys[pygame.K_d]:
-            self.set_rect(self.get_rect().move(1 * self.get_step(), 0))
+            self.move((1 * self.get_step(), 0))
 
-        self.set_rect(self.get_rect().move(*self.get_vector()))
+
+        self.move(self.get_vector())
 
         self.normalize_pos()
         self.set_vector((
             self.get_vector()[0], self.get_vector()[1] + self.get_g()))
 
     def jump(self):
-        pass
+        if self.get_hook():
+            self.get_rect().y = 0
+
+    def move(self, vector: tuple):
+        """Двигает на заданный вектор"""
+        shift = 0, 0
+        steps = max(map(lambda value: abs(round(value)), vector)) + 1
+        shift = shift[0] + round(vector[0] / steps), \
+                shift[1] + round(vector[1] / steps)
+
+        # Разделяет большой шаг на меньшие, длиной максимум в 1 пиксель, нужен
+        # для того, чтобы при большом значении вектора спрайт не
+        # телепортировался на этот вектор и не пропускал все препятствия на
+        # своем пути
+        for i in range(steps, 0, -1 if steps > 0 else 1):
+            next_pos = \
+                shift[0] + self.get_rect().x, shift[1] + self.get_rect().y
+
+            if not self.check_collides(next_pos):
+                break
+
+    def check_collides(self, pos: tuple) -> bool:
+        collided_sprite_by_mask = self.temp_move(
+            pos, self.get_collided, pygame.sprite.collide_mask, self)
+        collided_sprite_by_bottom = self.temp_move(
+            pos, self.get_collided, pygame.sprite.collide_rect,
+            self.get_bottom)
+
+        # Если нижний хитбокс задел препятствие, это значит что спрайт
+        # коснулся пола, в этом случае нужно обнулить вектор и остановить
+        # движение
+        if isinstance(collided_sprite_by_bottom, Wall):
+            self.set_vector((0, 0))
+            return False
+
+        # Если маска задела препятствие, а нижний хитбокс нет, это значит
+        # что спрайт находится параллельно от препятствия
+        elif isinstance(collided_sprite_by_mask, Wall) \
+                and not isinstance(collided_sprite_by_bottom, Wall):
+            self.set_hook(True)
+            return False
+
+        # Иначе просто двигаемся дальше
+        else:
+            self.set_hook(False)
+            self.get_rect().x = pos[0]
+            self.get_rect().y = pos[1]
+            return True
+
+    def temp_move(self, pos: tuple, func, *args) -> move:
+        """Временно перемещает себя на указанную позицию и выполняет заданную
+         функцию в промежутке"""
+        real_pos = self.get_rect().x, self.get_rect().y
+        self.get_rect().x, self.get_rect().y = pos
+
+        if args[1] == self.get_bottom:
+            return_value = func(args[0], self.get_bottom())
+        else:
+            return_value = func(*args)
+
+        self.get_rect().x, self.get_rect().y = real_pos
+        return return_value
+
+    def get_collided(self, func, sprite1: pygame.sprite.Sprite):
+        collided_sprite = False
+        for sprite in self.get_all_sprites_group():
+            if func(sprite1, sprite):
+                collided_sprite = sprite
+        return collided_sprite
+
+    def get_bottom(self) -> pygame.sprite.Sprite:
+        """Возвращает спрайт - "низ игрока", нужен для различия между
+        скольжением около стены и падением на нее сверху. Возвращает именно
+        спрайт, а не Rect потому что в pygame нет функции для коллайда
+        Rect'ов"""
+
+        x, y = self.get_rect().x, self.get_rect().y
+        w, h = self.get_rect().size
+
+        padding = 20
+        bottom = pygame.sprite.Sprite()
+        bottom.rect = pygame.Rect(
+            x + padding, y + h, w - padding * 2, 1)
+
+        return bottom
+
+    def normalize_pos(self):
+        rect_w, screen_w = \
+            self.get_rect().width, self.get_parent().get_rect().width
+        self.rect.x = \
+            screen_w - rect_w if self.get_rect().x < 0 else self.get_rect().x
+        self.rect.x = \
+            0 if self.get_rect().x + rect_w > screen_w else self.get_rect().x
+
+        rect_h, screen_h = \
+            self.get_rect().height, self.get_parent().get_rect().height
+        self.get_rect().y = \
+            screen_h - rect_h if self.get_rect().y < 0 else self.get_rect().y
+        self.get_rect().y = \
+            0 if self.get_rect().y + rect_h > screen_h else self.get_rect().y
 
     def get_all_sprites_group(self) -> pygame.sprite.Group:
         return self.get_parent().get_sprites_without_hero_group()
@@ -64,12 +169,6 @@ class Hero(Sprite):
 
     def get_hp_info(self) -> tuple:
         return self.hp_info
-
-    def set_rect(self, rect: pygame.Rect):
-        self.rect = rect
-
-    def get_rect(self) -> pygame.Rect:
-        return self.rect
 
     def get_step(self) -> int:
         return self.step
@@ -90,20 +189,12 @@ class Hero(Sprite):
     def get_inertia(self) -> tuple:
         return self.inertia
 
-    def normalize_pos(self):
-        rect_w, screen_w = \
-            self.get_rect().width, self.get_parent().get_rect().width
+    def get_image(self) -> pygame.Surface:
+        return self.image
 
-        self.rect.x = \
-            screen_w - rect_w if self.get_rect().x < 0 else self.get_rect().x
-        self.rect.x = \
-            0 if self.get_rect().x + rect_w > screen_w else self.get_rect().x
+    def set_hook(self, hook: bool):
+        self.hook = hook
+        return self
 
-        rect_h, screen_h = \
-            self.get_rect().height, self.get_parent().get_rect().height
-        self.get_rect().y = \
-            screen_h - rect_h if self.get_rect().y < 0 else self.get_rect().y
-        self.get_rect().y = \
-            0 if self.get_rect().y + rect_h > screen_h else self.get_rect().y
-
-        # TODO Столкновение с непроходимыми объектами
+    def get_hook(self) -> bool:
+        return self.hook
--- src/constants/main_settings.py
+++ src/constants/main_settings.py
@@ -2,7 +2,7 @@ from constants.gui.colors import BLACK
 from constants.windows.windows_names import START_WINDOW_NAME
 
 FPS = 60
-SIZE = WIDTH, HEIGHT = 400, 500
+SIZE = WIDTH, HEIGHT = 800, 500
 BACKGROUND_COLOR = BLACK
 START_WINDOWS_NAMES = [START_WINDOW_NAME]
 TITLE = 'Square Jumper'
