0d56175cee50dfb6430f878217911e61b38404cf
ScareTrow <75736890+ScareTrow@users.noreply.github.com>
2022-01-29 23:01:40 +0600

    Немного обновил настройки для спрайта Hero
    Чтобы было проще включать анимацию, решил перенести функционал из Animations в Sprites

--- src/constants/sprites/hero/hero_settings.py
+++ src/constants/sprites/hero/hero_settings.py
@@ -5,6 +5,18 @@ from Sprites.Door.Door import Door
 
 IMAGE_PATH = 'data/images/sprites/hero/hero.png'
 STEP = 200  # Пикселей в секунду
-OBSTACLES = [Wall, Cannon]
+OBSTACLES = [Wall, Cannon, Bullet]
 ENEMIES = [Bullet]
 DESTINATIONS = [Door]
+
+set_index = 0
+frame_set_start = set_index * 3
+frame_set_end = frame_set_start + 2
+
+speed = 5
+dwell = 5
+
+running_right = 2
+running_left = 20
+idle_right = 29
+idle_left = 11
