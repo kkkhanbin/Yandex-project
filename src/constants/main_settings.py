7c88ab14c5260ac51585066926732925b8cbb5ab
khanbin <totoshka1588@gmail.com>
2022-01-29 23:08:05 +0600

    Добавил открытие уровней после прохождения других

--- src/constants/main_settings.py
+++ src/constants/main_settings.py
@@ -2,7 +2,7 @@ from constants.gui.colors import BLACK
 from constants.windows.windows_names import START_WINDOW_NAME
 
 FPS = 60
-SIZE = WIDTH, HEIGHT = 800, 500
+SIZE = WIDTH, HEIGHT = 1200, 900
 BACKGROUND_COLOR = BLACK
 START_WINDOWS_NAME = START_WINDOW_NAME
 TITLE = 'Square Jumper'
