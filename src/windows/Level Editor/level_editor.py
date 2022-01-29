9610be986efb5acffd2bbdfa3c18e038e5bba496
khanbin <totoshka1588@gmail.com>
2022-01-30 02:37:36 +0600

    Создал main.exe

--- src/windows/Level Editor/level_editor.py
+++ src/windows/Level Editor/level_editor.py
@@ -170,7 +170,8 @@ class Editor(Window):
             self.draw_background()
             self.draw_grid()
             self.draw_world()
-            self.draw_text(f"Уровень: {self.level}", self.font, WHITE, 10, SCREEN_HEIGHT + LOWER_MARGIN - 90)
+            self.draw_text(f"Уровень: {self.level}", self.font, WHITE, 10,
+                           SCREEN_HEIGHT + LOWER_MARGIN - 90)
             self.draw_text("Нажмите вверх или вниз, чтобы изменить уровень",
                            self.font, WHITE, 10,
                            SCREEN_HEIGHT + LOWER_MARGIN - 60)
@@ -178,14 +179,16 @@ class Editor(Window):
             # сохранение и загрузка данных
             if self.save_button.draw(screen):
                 # сохранение данных об уровне
-                with open(f"level{self.level}_data.csv", "w", newline="") as csvfile:
+                with open(f"level{self.level}_data.csv", "w", newline="") \
+                        as csvfile:
                     writer = csv.writer(csvfile, delimiter=",")
                     for row in self.world_data:
                         writer.writerow(row)
             # загрузка данных об уровне
             if self.load_button.draw(screen):
                 self.scroll = 0
-                with open(f"level{self.level}_data.csv", newline="") as csvfile:
+                with open(f"level{self.level}_data.csv", newline="") \
+                        as csvfile:
                     reader = csv.reader(csvfile, delimiter=",")
                     for x, row in enumerate(reader):
                         for y, tile in enumerate(row):
